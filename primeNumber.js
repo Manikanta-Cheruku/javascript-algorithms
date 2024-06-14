4e9277b4088d5ecaa0d92fbdb481370c06ef9241
manikantacheruku <manikantacheruku0308@gmail.com>
2024-06-14 17:22:35 +0530

    first commit

---
+++ primeNumber.js
@@ -0,0 +1,19 @@
+function primeNum(n) {
+    if (n < 1) {
+        return false;
+    }
+
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+
+console.log(primeNum(1)); // true
+console.log(primeNum(4)); // false
+console.log(primeNum(5)); // true
